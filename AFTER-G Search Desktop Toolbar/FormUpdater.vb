a1f3a60fd719dc56c2a6d2f27e6973aeb90c681d
Quentin Pugeat <quentin.pugeat@hotmail.fr>
2018-09-23 11:35:36 +0200

    Mise à jour des URL de SmartNet Apps Updater

--- AFTER-G Search Desktop Toolbar/FormUpdater.vb
+++ AFTER-G Search Desktop Toolbar/FormUpdater.vb
@@ -15,8 +15,8 @@ Public Class FormUpdater
             Dim MAJ2 As New WebClient
             AddHandler MAJ2.DownloadProgressChanged, AddressOf MAJ2_ProgressChanged
             AddHandler MAJ2.DownloadFileCompleted, AddressOf MAJ2_DownloadCompleted
-            Dim DownloadLink As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/downloads/smartnet-searchbar/smartnetsearchbar-download.txt")
-            Dim DerniereVersion As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/downloads/smartnet-searchbar/smartnetsearchbar-version.txt")
+            Dim DownloadLink As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/smartnetapps/updater/searchbar/windows/download.txt")
+            Dim DerniereVersion As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/smartnetapps/updater/searchbar/windows/version.txt")
             MAJ2.DownloadFileAsync(New Uri(DownloadLink), My.Computer.FileSystem.SpecialDirectories.Temp + "\smartnetsearchbar-update-" + DerniereVersion + ".exe")
         Catch ex As Exception
             MsgBox("Téléchargement impossible : " + ex.Message, MsgBoxStyle.Critical, "Téléchargement de la mise à jour")
@@ -32,7 +32,7 @@ Public Class FormUpdater
 
     Private Sub MAJ2_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
         Dim MAJ As New WebClient
-        Dim DerniereVersion As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/downloads/smartnet-searchbar/smartnetsearchbar-version.txt")
+        Dim DerniereVersion As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/smartnetapps/updater/searchbar/windows/version.txt")
         Process.Start(My.Computer.FileSystem.SpecialDirectories.Temp + "\smartnetsearchbar-update-" + DerniereVersion + ".exe", "/SILENT /NOCANCEL /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS")
         Me.Close()
         Application.Exit()
@@ -40,7 +40,7 @@ Public Class FormUpdater
 
     Private Sub FormUpdater_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim MAJ As New WebClient
-        Dim Nouveautes As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/downloads/smartnet-searchbar/smartnetsearchbar-nouveautes.txt")
+        Dim Nouveautes As String = MAJ.DownloadString("http://quentinpugeat.pagesperso-orange.fr/smartnetapps/updater/searchbar/windows/releasenotes.txt")
         RichTextBox1.Text = Nouveautes
     End Sub
 End Class
